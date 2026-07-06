c3924296ca455be04291773168daecca280862f1
Snehal <snehalmundhe208@gmail.com>
2026-07-06 21:41:01 +0530

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,17 +21,11 @@ const allowedOrigins = Array.from(new Set([
   'http://127.0.0.1:5173'
 ]));
 app.use(cors({
-  origin: (origin, callback) => {
-    // allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: true,
   credentials: true
 }));
+
+app.options('*', cors());
 
 // Logger HTTP Requests
 app.use(morgan('dev', {
